5c7a66bb2a6c8b042e83c179892681c38a47218d
Paola2005 <velascopaola2005@gmail.com>
2023-12-04 14:17:44 -0500

    feat: :tada: MultiplicacionRusa

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+class Multiplicacion
+{
+    public static void Main()
+    {
+        Console.Write("Ingrese el multiplicando: ");
+        if (int.TryParse(Console.ReadLine(), out int multiplicando))
+        {
+            Console.Write("Ingrese el multiplicador: ");
+            if (int.TryParse(Console.ReadLine(), out int multiplicador))
+            {
+                int resultado = MultiplicacionRusa(multiplicando, multiplicador);
+                Console.WriteLine($"El resultado de {multiplicando} * {multiplicador} es: {resultado}");
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingrese un multiplicador válido.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Por favor, ingrese un multiplicando válido.");
+        }
+    }
+
+    public static int MultiplicacionRusa(int multiplicando, int multiplicador)
+    {
+        int resultado = 0;
+
+        while (multiplicador > 0)
+        {
+            if (multiplicador % 2 != 0)
+            {
+                resultado += multiplicando;
+            }
+
+            multiplicando *= 2;
+            multiplicador /= 2;
+        }
+
+        return resultado;
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,31 @@
+# Método de Multiplicación Rusa
+
+El método de multiplicación rusa es una técnica antigua que utiliza la multiplicación y la división por 2 para calcular el producto de dos números de manera eficiente.
+
+## Descripción del Método
+
+El procedimiento consiste en multiplicar sucesivamente por 2 el multiplicando y dividir por 2 el multiplicador hasta que el multiplicador tome el valor de 1. Luego, sumamos los valores del multiplicando correspondientes a los multiplicadores impares.
+
+## Uso del Programa
+
+### Requisitos
+
+- Tener un entorno de desarrollo C# instalado
+
+### Instrucciones
+
+1. Ejecuta el programa en tu entorno de desarrollo C#.
+2. Ingresa el multiplicador y multiplicando cuando se solicite.
+
+El programa utilizará el método de multiplicación rusa para calcular el resultado de la multiplicación y mostrará el resultado final.
+
+## Ejemplo de Ejecución
+
+```
+Ingrese el multiplicador: 27
+Ingrese el multiplicando: 15
+
+Resultado de la multiplicación utilizando el método de multiplicación rusa: 405
+```
+
+**¡Experimenta con diferentes valores y explora la eficiencia del método de multiplicación rusa!**
